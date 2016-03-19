cdb19a29f2d5c8424da2cf265b6dac73b6505512
Paul Baecher <pbaecher@gmail.com>
2016-03-19 01:12:21 +0100

    finish MVP

--- mklabels/main.py
+++ mklabels/main.py
@@ -1,8 +1,39 @@
+import os
 from argparse import ArgumentParser
+from shutil import rmtree
+from subprocess import STDOUT, check_output
+from tempfile import mkdtemp
+from textwrap import dedent
 
 
-class NotEnoughSpace(Exception):
+# A4 landscape
+PAPER_WIDTH_MM = 297
+PAPER_HEIGHT_MM = 210
+
+
+class ImpossibleLayout(Exception):
     pass
+
+
+class TooManyLabels(Exception):
+    pass
+
+
+def quote_latex(s):
+    charmap = {
+        '&': r'\&',
+        '%': r'\%',
+        '$': r'\$',
+        '#': r'\#',
+        '_':  r'\_',
+        '{':  r'\{',
+        '}':  r'\}',
+        '~':  r'\textasciitilde{}',
+        '^':  r'\^{}',
+        '\\': r'\textbackslash{}',
+    }
+
+    return ''.join((charmap.get(c, c) for c in s))
 
 
 def layout(page_width, page_height, margin, label_width, label_height,
@@ -13,21 +44,121 @@ def layout(page_width, page_height, margin, label_width, label_height,
         (label_width + 2 * marker_length + 2 * marker_sep)
 
     if rows < 1 or columns < 1:
-        raise NotEnoughSpace(
-            'Not enough space on page to fit even one row or column!')
+        raise ImpossibleLayout(
+            'Not enough space on page to fit even one row or column')
 
     return (rows, columns)
 
 
 def render(labels, margin, width, height, marker_length, marker_sep):
-    pass
+    (rows, columns) = layout(PAPER_WIDTH_MM, PAPER_HEIGHT_MM, margin,
+                             width, height, marker_length, marker_sep)
+
+    if len(labels) > rows * columns:
+        raise TooManyLabels('Cannot fit this many labels on the page')
+
+    tex = dedent(r"""
+        \documentclass{standalone}
+        \usepackage[utf8]{inputenc}
+        \usepackage[T1]{fontenc}
+
+        \usepackage{tikz}
+        \usetikzlibrary{calc}
+        \tikzset{x=1mm,y=1mm}
+        \pgfmathsetmacro{\margin}{%(margin)d}
+        \pgfmathsetmacro{\markerlength}{%(marker_length)d}
+        \pgfmathsetmacro{\markersep}{%(marker_sep)d}
+        \pgfmathsetmacro{\labelwidth}{%(width)d}
+        \pgfmathsetmacro{\labelheight}{%(height)d}
+
+        \begin{document}
+        \begin{tikzpicture}
+        \useasboundingbox (0,0) rectangle (%(page_width)d,%(page_height)d);
+    """) % {
+        'margin': margin,
+        'marker_length': marker_length,
+        'marker_sep': marker_sep,
+        'width': width,
+        'height': height,
+        'page_width': PAPER_WIDTH_MM,
+        'page_height': PAPER_HEIGHT_MM,
+    }
+
+    x = 0
+    y = 0
+
+    for label in labels:
+        tex += dedent(r"""
+            \pgfmathsetmacro{\xpos}{\margin + %(x)d * (
+              \labelwidth + 2 * \markerlength + 2 * \markersep)};
+            \pgfmathsetmacro{\ypos}{\margin + %(y)d * (
+              \labelheight + 2 * \markerlength + 2 * \markersep)};
+
+            %% debugging help lines
+            %%\draw[red] (\xpos,\ypos) rectangle +(\labelwidth,\labelheight);
+            %%\draw[blue] ($(\xpos,\ypos + 1/2 * \labelheight)$) --
+            %%  +(\labelwidth,0);
+
+            \draw (\xpos,\ypos) ++(0,-\markersep) -- ++(0,-\markerlength);
+            \draw (\xpos,\ypos) ++(-\markersep,0) -- ++(-\markerlength,0);
+
+            \draw ($(\xpos,\ypos + \labelheight)$) ++(0,\markersep) --
+              ++(0,\markerlength);
+            \draw ($(\xpos,\ypos + \labelheight)$) ++(-\markersep,0) --
+              ++(-\markerlength,0);
+
+            \draw ($(\xpos + \labelwidth,\ypos)$) ++(0,-\markersep) --
+              ++(0,-\markerlength);
+            \draw ($(\xpos + \labelwidth,\ypos)$) ++(\markersep,0) --
+              ++(\markerlength,0);
+
+            \draw ($(\xpos + \labelwidth,\ypos + \labelheight)$)
+              ++(0,\markersep) -- ++(0,\markerlength);
+            \draw ($(\xpos + \labelwidth,\ypos + \labelheight)$)
+              ++(\markersep,0) -- ++(\markerlength,0);
+
+            \node[font=\sffamily\LARGE] at
+              ($(\xpos + 1/2 * \labelwidth, \ypos + 1/2 * \labelheight)$)
+              {\strut %(label)s};
+        """) % {
+            'x': x,
+            'y': y,
+            'label': quote_latex(label),
+        }
+
+        if (x + 1) < columns:
+            x += 1
+        else:
+            x = 0
+            y += 1
+
+    tex += dedent(r"""
+        \end{tikzpicture}
+        \end{document}
+    """)
+
+    return tex
+
+
+def compile_pdf(latex):
+    directory = mkdtemp(prefix='mklabels-')
+    try:
+        os.chdir(directory)
+        with file('labels.tex', 'w') as f:
+            f.write(latex)
+        check_output(
+            ['pdflatex', 'labels.tex', '-halt-on-error'], stderr=STDOUT)
+        check_output(['xdg-open', 'labels.pdf'])
+    finally:
+        rmtree(directory)
 
 
 def parse_args():
     parser = ArgumentParser()
+    parser.add_argument('labels', metavar='LABEL', nargs='+')
     parser.add_argument('--height', '-H', type=int, default=13, help=''
                         'height of the labels (mm)')
-    parser.add_argument('--latex', '-t', action='store_true', help=''
+    parser.add_argument('--latex-only', '-t', action='store_true', help=''
                         'output latex code only; don\'t compile')
     parser.add_argument('--marker-length', '-l', type=int, default=2, help=''
                         'length of cut markers (mm)')
@@ -43,3 +174,11 @@ def parse_args():
 
 def run():
     args = parse_args()
+    output = render(
+        args.labels, args.page_margin, args.width, args.height,
+        args.marker_length, args.marker_sep)
+
+    if args.latex_only:
+        print(output)
+    else:
+        compile_pdf(output)
--- mklabels/tests/test_main.py
+++ mklabels/tests/test_main.py
@@ -1,9 +1,27 @@
 import unittest
 
-from ..main import layout
+from ..main import ImpossibleLayout, TooManyLabels, layout, render
 
 
 class MainTestCase(unittest.TestCase):
     def test_layout_normal(self):
         (rows, columns) = layout(297, 210, 15, 56, 13, 2, 1)
         self.assertEqual((rows, columns), (9, 4))
+
+    def test_layout_maximal(self):
+        (rows, columns) = layout(297, 210, 15, 261, 174, 2, 1)
+        self.assertEqual((rows, columns), (1, 1))
+
+    def test_layout_width_error(self):
+        with self.assertRaises(ImpossibleLayout):
+            (rows, columns) = layout(297, 210, 15, 262, 174, 2, 1)
+
+    def test_layout_height_error(self):
+        with self.assertRaises(ImpossibleLayout):
+            (rows, columns) = layout(297, 210, 15, 261, 175, 2, 1)
+
+    def test_render(self):
+        render(['one'], 15, 261, 174, 2, 1)
+
+        with self.assertRaises(TooManyLabels):
+            render(['one', 'two'], 15, 261, 174, 2, 1)
